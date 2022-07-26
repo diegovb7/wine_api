18fd586de02316c225163e2549ae83059a846cfb
diegovb7 <dvb7@alu.ua.es>
2022-07-26 16:23:36 +0200

    getall

--- src/test/java/com/wine_api/wine_api/controller/WineApiControllerTest.java
+++ src/test/java/com/wine_api/wine_api/controller/WineApiControllerTest.java
@@ -1,10 +1,15 @@
 package com.wine_api.wine_api.controller;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.wine_api.wine_api.repository.WineRepository;
 import com.wine_api.wine_api.wines.Region;
@@ -28,6 +33,14 @@ class WineApiControllerTest {
         //String acidity, Winery winery, Type type, Region region)
 
         Wine wine1 = new Wine("Wine1", "2000", new Float(4.5),
-             new Integer(4), new Float(6.7), "NA", "NA", new Winery("Winery1"), new Type("Duro"), new Region("España", "Europa"));
+            new Integer(4), new Float(6.7), "NA", "NA", new Winery("Winery1"), new Type("Duro"), new Region("España", "Europa"));
+
+        Wine wine2 = new Wine("Wine2", "2002", new Float(4.5),
+            new Integer(4), new Float(6.7), "NA", "NA", new Winery("Winery2"), new Type("Duro2"), new Region("España", "Europa"));
+        
+        Mockito.when(wineRepository.findAll()).thenReturn(Arrays.asList(wine1, wine2));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/wine"))
+                .andExpect(MockMvcResultMatchers.jsonPath("@.[0].id").value(1));
     }
 }
