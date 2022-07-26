6ebad5cc81e56545b54aefbb430e03d9ce6b62b2
Paco Garcia <focg@gft.com>
2022-07-26 11:23:24 +0200

    Repositorios creados

---
+++ src/main/java/com/wine_api/wine_api/repository/WineRepository.java
@@ -0,0 +1,11 @@
+package com.wine_api.wine_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wine_api.wine_api.wines.Wine;
+
+@Repository
+public interface WineRepository extends JpaRepository<Wine, Long>{
+
+}
