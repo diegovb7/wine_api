a16e802c84c459ea8d4783604e42f98a3cad98f7
diegovb7 <dvb7@alu.ua.es>
2022-07-26 11:11:25 +0200

    final clase region

--- src/main/java/com/wine_api/wine_api/wines/Region.java
+++ src/main/java/com/wine_api/wine_api/wines/Region.java
@@ -17,4 +17,40 @@ public class Region implements Serializable{
 
     @OneToMany(mappedBy = "winery", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Set<Wine> wines;
+
+    public Region(){
+        
+    }
+
+    public Region(String name, String country, Set<Wine> wines) {
+        this.name = name;
+        this.country = country;
+        this.wines = wines;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Set<Wine> getWines() {
+        return wines;
+    }
+
+    public void setWines(Set<Wine> wines) {
+        this.wines = wines;
+    }
+
+    
 }
