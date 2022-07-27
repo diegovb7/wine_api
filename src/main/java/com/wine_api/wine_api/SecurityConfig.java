32ec92179652fd46493c26cb414f84482332b77a
PacoGarcia79 <fco.garcia.delgado@gmail.com>
2022-07-27 17:26:43 +0200

    Actuator y primera fase security

--- src/main/java/com/wine_api/wine_api/SecurityConfig.java
+++ src/main/java/com/wine_api/wine_api/SecurityConfig.java
@@ -9,21 +9,35 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.Customizer;
+
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+		// http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+
+		http.authorizeHttpRequests((authz) -> authz
+				.antMatchers(HttpMethod.GET).permitAll()
+				.antMatchers(HttpMethod.POST, "/**").hasRole("USER")
+				.antMatchers(HttpMethod.PUT, "/**").hasRole("USER")
+				.antMatchers(HttpMethod.PATCH, "/**").hasRole("USER")
+				.antMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN"))
+				.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable()
+				.httpBasic(Customizer.withDefaults());
 	}
 
 	@Bean
 	@Override
 	public UserDetailsService userDetailsService() {
-		UserDetails user = User.withDefaultPasswordEncoder().username("admin").password("1234").roles("USER")
+		UserDetails user = User.withDefaultPasswordEncoder().username("user").password("1234").roles("USER").build();
+
+		UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("5678").roles("USER", "ADMIN")
 				.build();
 
 		return new InMemoryUserDetailsManager(user);
 	}
 }
-
-
